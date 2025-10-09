3b9029056f8bb6e4f113032b03aa7b4c2f08422f
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-10 05:31:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
