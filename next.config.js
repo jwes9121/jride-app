6c6080ab44574ec87869869be1359c18688f606b
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-10 07:08:07 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // Explicit aliases to avoid "Module not found" for @components/... on Vercel
-    config.resolve.alias["@components"] = path.join(__dirname, "components");
-    config.resolve.alias["@lib"] = path.join(__dirname, "lib");
-    config.resolve.alias["@hooks"] = path.join(__dirname, "hooks");
-    config.resolve.alias["@app"] = path.join(__dirname, "app");
-    config.resolve.alias["@"] = path.join(__dirname);
-    return config;
+  experimental: {
+    // App Router (default in Next 14)
   }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
