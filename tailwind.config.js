e67f9309d7813ab500ade2ad9e418513db5048b5
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-10 05:57:25 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
