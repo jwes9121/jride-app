29820524290b12ff163a15a52f73dc6ceef59605
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-09 08:24:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Never fail CI builds due to lint or type errors
   eslint: { ignoreDuringBuilds: true, dirs: [] },
   typescript: { ignoreBuildErrors: true }
 };
