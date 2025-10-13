3b97e54b11fdaf35a3f47a0554bb68262e076ca6
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-14 06:46:34 +0800

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Important: do NOT set output: "export" (that disables API routes)
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;
