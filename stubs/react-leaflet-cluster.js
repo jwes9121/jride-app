e55d1074ca66e41da92428b656f2b58efd2f35b5
jwes9121 <235419512+jwes9121@users.noreply.github.com>
2025-10-14 08:49:41 +0800

    Create react-leaflet-cluster.js

---
+++ stubs/react-leaflet-cluster.js
@@ -0,0 +1,6 @@
+// Minimal no-op stub for 'react-leaflet-cluster'
+const React = require('react');
+
+module.exports = function MarkerClusterGroup(props) {
+  return React.createElement('div', props, props.children);
+};
